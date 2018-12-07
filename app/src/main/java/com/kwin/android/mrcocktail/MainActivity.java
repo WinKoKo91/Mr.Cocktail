e883284b9800e99cf0b8fad279282a635753b457
Win <winkoko@owaytrip.com>
2018-12-07 14:18:01 +0630

    first time

---
+++ app/src/main/java/com/kwin/android/mrcocktail/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kwin.android.mrcocktail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
